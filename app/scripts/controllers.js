38278693d82daa2f4bfa1f5aca40d1b64f603de2
Ian White <ianwhite@utexas.edu>
2014-05-26 20:50:24 -0500

    add initial modal-need to customize

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,8 +1,31 @@
 'use strict';
 angular.module('IonicWeatherApp.controllers', ['ionic'])
 
-.controller('DashCtrl', function($scope, $ionicLoading) {
-
+.controller('DashCtrl', function($scope, $ionicLoading, $ionicModal) {
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hide', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
   $scope.add = function(cityName, stateName) {
     console.log(cityName + ', ' + stateName);
     $ionicLoading.show({
