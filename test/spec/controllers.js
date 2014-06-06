ef7af0ee75415b4d21af9e02e24c7925a447ffcb
Ian White <ianwhite@utexas.edu>
2014-06-05 19:49:46 -0500

    initialize test file for controllers.js

--- test/spec/controllers.js
+++ test/spec/controllers.js
@@ -1,25 +1,25 @@
 'use strict';
 
-describe('Controller: PetIndexCtrl', function () {
+describe('Controller: DashCtrl', function () {
 
   var should = chai.should();
 
   // load the controller's module
-  beforeEach(module('IonicWeatherApp'));
+  beforeEach(module('IonicWeatherApp.controllers', 'IonicWeatherApp.services'));
 
-  var PetIndexCtrl,
+  var DashCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    PetIndexCtrl = $controller('PetIndexCtrl', {
+    DashCtrl = $controller('DashCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of pets to the scope', function () {
-    scope.pets.should.have.length(4);
+  it('should verify cities length', function () {
+    scope.createComment.should.have.length(4);
   });
 
 });
